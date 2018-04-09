9a8306f8221ea2f9afc8a7cf5d7175547b39f5e0
Handerson Contreras <hcontreras@hnsc.net>
2018-04-09 15:15:36 -0600

    updating version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='auth_jwt',
-      version='0.1.7',
+      version='0.1.8',
       description='Module to give secure the api with jwt token',
       url='https://github.com/handersonc/auth_jwt.git',
       author='Handerson Contreras',
